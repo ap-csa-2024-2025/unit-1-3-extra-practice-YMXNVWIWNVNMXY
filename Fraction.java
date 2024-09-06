f952658f4049c9fc3cd3f3842f4c36fd9f3ee4b1
YMXNVWIWNVNMXY <jnbui106@student.hbuhsd.edu>
2024-09-06 10:15:58 -0700

    aw

--- Fraction.java
+++ Fraction.java
@@ -1,1 +1,7 @@
-
+public class Pyramid
+{
+  public static void main(String[] args)
+  {
+    
+  }
+}
--- Pyramid.java
+++ Pyramid.java
@@ -2,6 +2,9 @@ public class Pyramid
 {
   public static void main(String[] args)
   {
-    // put code here
+    int numBlocks = 2500000;
+    double blockWeight = 2.5;
+    double pyramidWeight = numBlocks * blockWeight;
+    System.out.print("Kufu's pyramid in Giza is estimated to weigh " + pyramidWeight + " tons.");
   }
 }
