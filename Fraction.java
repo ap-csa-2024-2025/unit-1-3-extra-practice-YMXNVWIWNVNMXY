0e35673285bafb4afeb8f44a32ac002056a021ef
YMXNVWIWNVNMXY <jnbui106@student.hbuhsd.edu>
2024-09-07 00:49:16 +0000

    wa

--- Fraction.java
+++ Fraction.java
@@ -1,7 +1,21 @@
-public class Pyramid
+public class Fraction
 {
   public static void main(String[] args)
   {
-    
+    int nume1 = 1;
+    int nume2 = 2;
+    int deno1 = 3;
+    int deno2 = 4;
+    System.out.print("The numerator of the first fraction is " + nume1 + ".");
+    System.out.print("The denominator of the first fraction is " + deno1 + ".");
+    System.out.print("The numerator of the second fraction is " + nume2 + ".");
+    System.out.print("The denominator of the second fraction is " + deno2 + ".");
+    int topSum = nume1 + deno1 + nume2 + deno2;
+    int bottomSum = deno1 + deno2;
+    String frac1 = nume1 + "/" + deno1;
+    String frac2 = nume2 + "/" + deno2;
+    String sum = (nume1 + nume2) + "/" + (deno1 + deno2);
+    //"/"
+    System.out.print("The sum of " + frac1 + " + " + frac2 + " = " + sum + "." );
   }
 }
--- Gravity.java
+++ Gravity.java
@@ -1,1 +1,12 @@
-
+public class Gravity
+{
+  public static void main(String[] args)
+  {
+    final double g = 9.8; // i can change the value of g and time and get no errors
+    double time = 23;
+    double height = 0.5 * (g * (time * time));
+    double velocity = g * time;
+    System.out.println("The height of the cliff is " + height + " m.");
+    System.out.println("The final velocity of the ball is " + velocity + " m/s");
+  }
+}
